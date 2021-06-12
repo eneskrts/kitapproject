a3c36e7ba0c9c9a9d118628ee2279f37bfb19c5b
Enes <enskrts998@gmail.com>
2021-06-12 16:07:22 +0300

    Update settings.py

--- kitapproje/settings.py
+++ kitapproje/settings.py
@@ -142,8 +142,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'enskrts998@gmail.com'
-EMAIL_HOST_PASSWORD = 'Samsung799@?'
+EMAIL_HOST_USER = os.environ.get("email")
+EMAIL_HOST_PASSWORD = os.environ.get("password")
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
